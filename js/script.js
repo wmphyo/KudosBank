781e403cc1441389a710085d4852de898a7507b6
wmphyo <win.m.phyo@gmail.com>
2024-06-14 23:09:11 +0700

    Brand new UI, Brand new Site Map.

--- js/script.js
+++
@@ -1,8 +0,0 @@
-/*Toggle menu for mobile screen*/
-let navLinks = document.getElementById('navLinks');
-function showMenu(){
-    navLinks.style.right = '0';
-}
-function hideMenu(){
-    navLinks.style.right = '-200px';
-}
